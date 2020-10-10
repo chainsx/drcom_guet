435151db4fe5e1b302b32ccdd0d170bd14be1e4b
chainsx(cx-dandelion) <pzhrhlx@outlook.com>
2020-10-11 01:36:58 +0800

    靠，连夜修复笔误带来的bug
    靠，连夜修复笔误带来的bug

--- drcom_guet.sh
+++ drcom_guet.sh
@@ -3,4 +3,4 @@
 time=$(date +%s%3N)
 number=1900301019
 pwd=123456
-echo "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
+curl "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
