09f054187480e0492fcca5b2b426c5b5dbaac815
chainsx(cx-dandelion) <pzhrhlx@outlook.com>
2020-10-11 12:42:59 +0800

    Update drcom_guet.sh

--- drcom_guet.sh
+++ drcom_guet.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
 time=$(date +%s%3N)
-number=1900301019
+number=19003xxxxx
 pwd=123456
 curl "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
