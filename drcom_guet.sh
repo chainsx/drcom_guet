fbf4e5d7adbd6bd7743eb48fc461890a4a319349
chainsx(cx-dandelion) <pzhrhlx@outlook.com>
2020-12-15 18:36:45 +0800

    Update drcom_guet.sh

--- drcom_guet.sh
+++ drcom_guet.sh
@@ -1,6 +1,14 @@
 #!/bin/ash
-
 time=$(date +%s%3N)
 number=19003xxxxx
-pwd=123456
-curl "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
+pwd=xxxxxx
+HOST=10.70.121.254
+#touch /tmp/networkstatus.log
+ping -c 4 "$HOST" > /dev/null
+if [ $? -eq 0 ];then
+        echo "check final" >> /tmp/networkstatus.log
+else
+        curl "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
+        #touch /tmp/networkstatus.log
+        echo "final" >> /tmp/networkstatus.log
+fi
