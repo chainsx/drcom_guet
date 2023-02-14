4a4172862b077a68525f58e517f489873e3bf5a7
chainsx <pzhrhlx@outlook.com>
2023-02-14 13:42:37 +0800

    Update drcom_guet.sh

--- drcom_guet.sh
+++ drcom_guet.sh
@@ -8,7 +8,7 @@ ping -c 4 "$HOST" > /dev/null
 if [ $? -eq 0 ];then
         echo "check final" >> /tmp/networkstatus.log
 else
-        curl "http://10.32.254.11/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
+        curl "http://10.0.1.5/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
         #touch /tmp/networkstatus.log
         echo "final" >> /tmp/networkstatus.log
 fi
