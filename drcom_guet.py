d51d9986098ab1789f07892c84f83d4a55420e90
libfrozen.so <20748632+libfrozen@users.noreply.github.com>
2024-06-12 03:44:19 +0800

    change login url parameters to make things work again

--- drcom_guet.py
+++ drcom_guet.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-# @Author  : chainsx
+# @Author  : chainsx,libfrozen
 # @FileName: drcom_guet.py
 
-import time
 import json
 import requests
+import math
+import random
 from time import sleep
 from subprocess import run, PIPE
 
-user = "190030xxxx"
 #学号
-pwd = "xxxxxxxx"
+user = "2000******"
 #密码
-type = 3
+pwd = "NikaidouShinkuDaisuki"
 # 0,1,2,3分别对应校园网、移动、联通与电信。
+type = 3
 
 login_type=['', '@cmcc', '@unicom', '@telecom']
 
 def login(user, pwd, type):
     url = "http://10.0.1.5/drcom/login"
     data = {
-        'callback': 'dr' + str(int(time.time() * 1000)),
+        'callback': 'dr1003',
         'DDDDD': user + login_type[type],
         'upass': pwd,
         '0MKKey': '123456',
@@ -33,26 +34,27 @@ def login(user, pwd, type):
         'v6ip': None,
         'terminal_type': '1',
         'lang': 'zh-cn',
-        'jsVersion': '4.1',
-        'v': '6471',
+        'jsVersion': '4.2',
+        'v': math.floor(random.random()*10000+500),
+        'lang': "zh"
         }
     res = requests.get(url, params=data).text
     res = res.split('(')[1].split(')')[0]
     res = json.loads(res)
 
     if res['result'] == 1:
-        print(res['uid'] + " login success")
-        print("your ip address is " + res['v46ip'])
+        print(res['uid'] + " 登陆成功")
+        print("系统分配的IP地址是" + res['v46ip'])
 
 cnt = 1
 while True:
-    r = run('ping www.guet.edu.cn',
+    r = run('ping 223.5.5.5',
             stdout=PIPE,
             stderr=PIPE,
             stdin=PIPE,
             shell=True)
     if r.returncode:
-        print('relogin 第{}次'.format(cnt))
+        print('登录尝试第{}次'.format(cnt))
         login(user, pwd, type)
         cnt += 1
     else:
--- drcom_guet.sh
+++ drcom_guet.sh
@@ -1,14 +1,49 @@
-#!/bin/ash
-time=$(date +%s%3N)
-number=19003xxxxx
-pwd=xxxxxx
-HOST=8.8.8.8
+#!/bin/bash
+uid=2000********
+carrier=3
+pwd=NikaidouShinkuDaisuki
+HOST=223.5.5.5
 #touch /tmp/networkstatus.log
+for((i=0;i<${#pwd};i++))
+do
+    case ${pwd:$i:1} in
+    \ )
+        pwd2=${pwd2}"%20";;
+    \")
+        pwd2=${pwd2}"%22";;
+    \#)
+        pwd2=${pwd2}"%23";;
+    \%)
+        pwd2=${pwd2}"%25";;
+    \&)
+        pwd2=${pwd2}"%26";;
+    \()
+        pwd2=${pwd2}"%28";;
+    \))
+        pwd2=${pwd2}"%29";;
+    \+)
+        pwd2=${pwd2}"%2B";;
+    \=)
+        pwd2=${pwd2}"%3D";;
+    \@)
+        pwd2=${pwd2}"%40";;
+    *)
+        pwd2=${pwd2}${pwd:$i:1};;
+    esac
+done
+case $carrier in
+1)  uid=${uid}"%40cmcc";;
+2)  uid=${uid}"%40unicom";;
+3)  uid=${uid}"%40telecom";;
+*)  uid=${uid};;
+esac
 ping -c 4 "$HOST" > /dev/null
 if [ $? -eq 0 ];then
         echo "check final" >> /tmp/networkstatus.log
 else
-        curl "http://10.0.1.5/drcom/login?callback=dr$time&DDDDD=$number&upass=$pwd&0MKKey=123456&R1=0&R3=1&R6=0&para=00&v6ip=&_=$time"
+        result=`curl "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=$uid&upass=$pwd2&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.2&v=$RANDOM&lang=zh"`
+        echo $result
         #touch /tmp/networkstatus.log
         echo "final" >> /tmp/networkstatus.log
 fi
+
